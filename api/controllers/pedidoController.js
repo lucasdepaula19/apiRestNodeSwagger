95dea09f9babcb5a93fdf083a92473379824ed9c
lucasdepaula19 <lucasdepaula2012@gmail.com>
2019-06-16 01:28:26 -0300

    Ajustes métodos

--- api/controllers/pedidoController.js
+++ api/controllers/pedidoController.js
@@ -24,51 +24,58 @@ function get(req, res) {
   res.json(lista);
   //res.json('GET funcionou!');
 }
+
 function getById(req, res) {
   let param_id = JSON.stringify(req.swagger.params.pedidoID.value);
 
   for (let i = 0; i < lista.length; i++) {
-      if(lista[i].id == param_id){
-          res.json(lista[i]);
-      }
+    if (lista[i].id == param_id) {
+      let zlista = JSON.stringify(lista[i]);
+      res.json(zlista);
+    }
   }
 }
+
 function post(req, res) {
   prodList.items.push(req.body)
   res.json('Produto criado!');
-  console.log(lista);
+  //console.log(lista);
 }
+
 function put(req, res) {
   let param_id = JSON.stringify(req.swagger.params.pedidoID.value);
   let param_body = req.body;
-  
-  for (let x = 0; x < lista.length; x++) {
-      if(lista[x].id == param_id){
-          if(param_body.tipo != undefined){
-              lista[x].tipo = param_body.tipo;
-          }
-          if(param_body.nome != undefined){
-              lista[x].nome = param_body.nome;
-          }
-          if(param_body.fabricante != undefined){
-              lista[x].fabricante = param_body.fabricante;
-          }
-          if(param_body.descricao != undefined){
-              lista[x].descricao = param_body.descricao;
-          }
-          
-          res.end('Produto atualizado!');
-      }
-  }
+
+res.json("id: "+ param_id + " body: "+param_body);
+
+  // for (let x = 0; x < lista.length; x++) {
+  //   if (lista[x].id == param_id) {
+  //     if (param_body.tipo != undefined) {
+  //       lista[x].tipo = param_body.tipo;
+  //     }
+  //     if (param_body.nome != undefined) {
+  //       lista[x].modelo = param_body.modelo;
+  //     }
+  //     if (param_body.fabricante != undefined) {
+  //       lista[x].fabricante = param_body.fabricante;
+  //     }
+  //     if (param_body.descricao != undefined) {
+  //       lista[x].descricao = param_body.descricao;
+  //     }
+
+  //     res.json('Produto atualizado!');
+  //   }
+  // }
 }
+
 function del(req, res) {
-  let zreq = JSON.stringify(req.swagger.params.pedidoID.value);
+  let zreq = JSON.parse(req.swagger.params.pedidoID.value);
 
   for (let i = 0; i < lista.length; i++) {
     if (lista[i].id === zreq) {
       lista.splice(i, 1);
       i--;
-      res.end('Produto excluído!');
+      res.json('Produto excluído!');
     }
   }
 }
